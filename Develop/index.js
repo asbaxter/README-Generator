58199a8d210aca4f9c962c290baf46c57d9515c7
Andrew Baxter <asbaxter1@gmail.com>
2022-07-24 19:55:14 -0400

    apllication now prompt user with all questions needed for the project

--- Develop/index.js
+++ Develop/index.js
@@ -2,27 +2,72 @@
 import inquirer from "inquirer";
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        name: 'project',
+        message: 'What is the title of you projects? ',
+        type: 'input'
+    },
+    {
+        name: 'github',
+        messgae: 'What is your GitHub user name?',
+        type: 'input'
+    },
+    {
+        name: 'email',
+        messgae: 'What is your email',
+        type: 'input'
+    },
+    {
+        name: 'description',
+        message: 'Describe your project: ',
+        type: 'input'
+    },
+    {
+        name: 'lincense',
+        message: 'What license did you use',
+        type: 'list',
+        choices: ['MIT', 'Apache 2.0', 'ISC', 'BSD', 'none']
+    },
+    {
+        name: 'installation',
+        messgae: 'Add the installation instructions: ',
+        type: 'input'
+    },
+    {
+        name: 'usage',
+        messgae: 'What is your applications use case?',
+        type: 'input'
+    },
+    {
+        name: 'contribution',
+        messgae: 'How can someone else contribute to this project? ',
+        type: 'input'
+    },
+    {
+        name: 'testing',
+        messgae: 'Add any testing intructions: ',
+        type: 'input'
+    },
+    {
+        name: 'usage',
+        messgae: 'What is you applications use case: ',
+        type: 'input'
+    },
+
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer
-        .prompt([
-            {
-                name: 'question1',
-                message: 'What is your name',
-                type: 'input'
-            }
-        ])
-        .then(function(answer){
-            console.log(answer);
-        })
+  inquirer.prompt(questions).then((data) => {
+    console.log(data);
+  });
 
-
-}
+};
 
 // Function call to initialize app
 init();
